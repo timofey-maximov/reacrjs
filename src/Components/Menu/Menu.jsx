ea5828bf99f02315e6d4d79131b3fc58a8559c94
timofey-maximov <timamaximov7@mail.ru>
2020-12-12 16:24:49 +0300

    removed unnecessary

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -18,25 +18,28 @@ export function Menu(props) {
 
     return (
         <div id="header">
-            <div>
-                <div id="nav-container">
-                    <div id="logo">
-                        <a href="#"><img src="favicon.png" alt="logo" width="40px"></img></a>
-                        <a href="#" title="На главную">MovieFinder</a>
-                    </div>
+            <div id="nav-container">
 
-                    <div id="nav-wrapper">
-
-                        <ul id="nav">
-
-                            {props.items.map((item, i) => (
-                                <li id="nav-item" key={i} className={getItemClassName(i)} onClick={() => processItemClick(i)}>{item}</li>
-                            ))}
-
-                        </ul>
-
-                    </div>
+                <div id="logo">
+                    <a href="#"><img src="favicon.png" alt="logo" width="40px"></img></a>
+                    <a href="#" title="На главную">MovieFinder</a>
                 </div>
+
+
+                <div id="nav-wrapper">
+
+                    <ul id="nav">
+                        {props.items.map((item, i) => (
+                            <li id="nav-item" key={i} className={getItemClassName(i)} onClick={() => processItemClick(i)}>{item}</li>
+                        ))}
+                    </ul>
+
+                    <div id="menu-togle" id="menu-icon">
+                        <div id="menu-icon-line"></div>
+                    </div>
+
+                </div>
+                
             </div>
         </div>
     );
